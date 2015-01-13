238c81dd3b40ea268d2e86a792db91f898a7362c
4johnny <4johnny@users.noreply.github.com>
2015-01-13 14:50:10 -0800

    Initial Commit

---
+++ Boxes/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Boxes
+//
+//  Created by Johnny on 2015-01-13.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
